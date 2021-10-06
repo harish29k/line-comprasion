d7f627564b18d5f15dd50da5ccfee2b7fb354bf6
harish29k <90303660+harish29k@users.noreply.github.com>
2021-10-06 11:37:02 +0530

    welcome line program

---
+++ Welcometolinecomprasion.java
@@ -0,0 +1,12 @@
+/**
+ * Start with DisplayingWelcome to Line
+Comparison ComputationProgram on Master Branch
+ */
+package linecomprasion;
+
+  public class Welcometolinecomprasion {
+  public static void main(String[] args) {
+	  System.out.println("welcome to line comparatarison computation program");
+  }
+
+}
